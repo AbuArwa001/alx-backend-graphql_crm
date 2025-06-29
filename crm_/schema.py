631998683b40932656fb89158316781449e0f77b
AbuArwa001 <khalfanathman12@yahoo.com>
2025-06-29 11:58:42 +0300

    feat: Initialize Django project with GraphQL integration
    - Added Django settings for the CRM project.
    - Created URL configuration for admin and GraphQL endpoints.
    - Implemented WSGI and ASGI configurations.
    - Set up basic structure for the CRM app including models, admin, and views.
    - Defined GraphQL schema with types and mutations for Customer, Product, and Order.
    - Added requirements.txt for project dependencies.
    - Created manage.py for administrative tasks.

--- crm_/schema.py
+++ crm_/schema.py
@@ -1,10 +1,12 @@
 from graphene import ObjectType
-from graphene import String, Int, Field, List
+from graphene import String
 from graphene import Schema
-from graphene import Mutation, InputObjectType
 
 class Query(ObjectType):
     hello = String(greetings=String(default_value="Hello, GraphQL"))
 
     def resolve_hello(self, info, greetings):
         return f"{greetings}!"
+
+# schema = Schema(query=Query)
+schema = Schema(query=Query)
